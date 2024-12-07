8ade3690dd094caf5a0de796ea500089d75b659d
Minsu Son <minsuson@Minsuui-MacBookAir.local>
2024-12-07 14:09:49 +0900

    setting

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM runpod/pytorch:2.1.0-py3.10-cuda11.8.0-devel-ubuntu22.04
 RUN apt-get update && apt-get install -y \
 	vim \
 
-RUN sh LangEye/build.sh
+RUN sh lang-env.sh
 
 
--- runpod_script.sh
+++ runpod_script.sh
@@ -6,10 +6,3 @@ git config --global user.email "nonsignal007@gmail.com"
 touch ~/.ssh/config
 echo "Host github.com
     IdentityFile ~/.ssh/runpod_key" >> ~/.ssh/config
-
-## git clone
-git clone git@github.com:nonsignal007/LangEyE.git
-cd LangEyE
-
-## dependency
-sh lang-env.sh
