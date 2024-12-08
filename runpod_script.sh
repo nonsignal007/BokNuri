016c67498dda762cdc92380b71acf6722a277497
nonsignal007 <nonsignal007@gmail.com>
2024-12-08 04:59:58 +0000

    tts test done

--- runpod_script.sh
+++
@@ -1,8 +0,0 @@
-## git setting
-git config --global user.name "nonsignal007"
-git config --global user.email "nonsignal007@gmail.com"
-
-## git ssh setting
-touch ~/.ssh/config
-echo "Host github.com
-    IdentityFile ~/.ssh/runpod_key" >> ~/.ssh/config
